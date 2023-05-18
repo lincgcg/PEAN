120a8ba4452c534ff58d9f339f622dd799cdb30d
“lincgcg <chunganglin@gmail.com>
2023-05-18 20:35:29 +0800

    debuf

--- train_eval.py
+++ train_eval.py
@@ -150,7 +150,7 @@ def evaluate(config, model, data_iter, test=False):
     if test:
         print("\nTime Difference (s): " + str(diff))
         print("Per Sample Use (s): " + str(diff / datasize) + "\n")
-        report = metrics.classification_report(labels_all, predict_all, target_names=config.class_list, digits=4)
+        report = metrics.classification_report(labels_all, predict_all, np.arange(0,len(config.class_list),1), target_names=config.class_list, digits=4)
         print(report)
         confusion = metrics.confusion_matrix(labels_all, predict_all)
         f1 = metrics.f1_score(labels_all, predict_all, average='macro')
